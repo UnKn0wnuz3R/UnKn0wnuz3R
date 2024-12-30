f52b3c880b73984b7d6f58dd8d7b9769e5a79eb4
UnKn0wnuz3R <128234584+UnKn0wnuz3R@users.noreply.github.com>
2024-12-30 20:09:39 +0900

    Delete Lab1 directory

--- Lab1/P5.fs
+++
@@ -1,7 +0,0 @@
-module P5
-
-/// For int list 'l' that contains decimal digits (0~9), return the integer that
-/// is represented by this list. For example, "digitsToInt [1; 3; 2]" must
-/// return 132 as a result. When the input list is empty, just return 0.
-let rec digitsToInt (l: int list) : int =
-  List.fold (fun head tail -> head*10 + tail) 0 l
